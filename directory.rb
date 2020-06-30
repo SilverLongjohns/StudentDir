34e74986a65471c6c6b7c05e7626046baa8112c4
EC2 Default User <ec2-user@ip-172-31-61-235.ec2.internal>
2020-06-30 14:16:51 +0000

    student array added

--- directory.rb
+++ directory.rb
@@ -1,19 +1,24 @@
 # Defined the student count as student_count
 student_count = 11
-
+# First, the list of students is set into an array
+students = [
+    "Dr. Hannibal Lecter",
+    "Darth Vader",
+    "Nurse Ratched",
+    "Michael Corleone",
+    "Alex DeLarge",
+    "The Wicked Witch of the West",
+    "Terminator",
+    "Freddy Krueger",
+    "The Joker",
+    "Joffrey Baratheon",
+    "Norman Bates"
+    ]
+    
 puts "The students of Villains Academy"
 puts "-----------"
-# First, the list of students is printed
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+students.each do |x|
+    puts x
+end
 # Next, the number of students is printed
 puts "Overall we have #{student_count} great students"
